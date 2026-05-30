3f111d80649a44955e38ae689a1013a9eaf7e7c0
bender <kudin.dima@gmail.com>
2026-05-30 15:50:16 +0000

    Update theme fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,7 @@ import Tag from "@/tag/Tag";
 import { getVisualEditScript } from "@/utils/visual-edit-script";
 import { Lato } from "next/font/google";
 import { Open_Sans } from "next/font/google";
+import { Libre_Baskerville } from "next/font/google";
 
 
 
@@ -43,12 +44,14 @@ export const metadata: Metadata = {
 };
 
 
+
+const libreBaskerville = Libre_Baskerville({
+  variable: "--font-libre-baskerville",
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
 const inter = Inter({
   variable: "--font-inter",
-  subsets: ["latin"],
-});
-const openSans = Open_Sans({
-  variable: "--font-open-sans",
   subsets: ["latin"],
 });
 
@@ -60,7 +63,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <ServiceWrapper>
-        <body className={`${inter.variable} ${openSans.variable} antialiased`}>
+        <body className={`${libreBaskerville.variable} ${inter.variable} antialiased`}>
           <Tag />
           {children}
           <script
